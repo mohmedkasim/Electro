b4be5295e007426027289d5b7d962b139e50dfa9
mohmedkasim <mk4957454@gmail.com>
2021-06-22 20:12:03 -1200

    add hostServices

---
+++ lib/HostServices.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+class HostServices {
+
+  // static Future<dynamic> login(username,password) async {
+  //
+  //   var data = {
+  //     "username": username,
+  //     "password": password
+  //   };
+  //   String  _url = "https://ip";
+  //   var result = await http.post( _url , body: jsonEncode(data)).timeout(Duration(seconds: 7), onTimeout: () {
+  //
+  //     return null;
+  //   });
+  //   return result;
+  // }
+}
